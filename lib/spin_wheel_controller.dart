31719c0eb8a1827062d5635c944e4fc4e704dd7f
Kagema Njoroge <reecejames934@gmail.com>
2025-11-25 10:27:23 +0300

    Reorder spin method and registerSpinCallback for improved readability

--- lib/spin_wheel_controller.dart
+++ lib/spin_wheel_controller.dart
@@ -1,6 +1,5 @@
 library spin_wheel;
 
-import 'package:meta/meta.dart';
 import 'package:flutter/foundation.dart';
 
 /// Controller for managing spin logic
@@ -14,16 +13,16 @@ class SpinWheelController {
   /// Notifies whether the wheel is currently spinning.
   final ValueNotifier<bool> isSpinning = ValueNotifier(false);
 
-  /// Initiates the spin animation to land on the specified [targetIndex].
-  void spin(int targetIndex) {
-    _spinCallback?.call(targetIndex);
-  }
-
-  // Called by the widget to register the callback
   /// Registers the internal spin callback. This is used by the [SpinWheel]
   /// widget and should not be called directly by application code.
   @protected
   void registerSpinCallback(SpinCallback callback) {
     _spinCallback = callback;
+  }
+
+  // Called by the widget to register the callback
+  /// Initiates the spin animation to land on the specified [targetIndex].
+  void spin(int targetIndex) {
+    _spinCallback?.call(targetIndex);
   }
 }
